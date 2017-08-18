faf0740a977d192d0ba8adef3d72dded820714b4
qianyilun <qianyilun@gmail.com>
2017-08-17 21:45:08 -0700

    Refact the QueueManager_SAP project

---
+++ src/main/java/com/allen/Main.java
@@ -0,0 +1,43 @@
+package com.allen;
+
+
+
+import com.allen.controller.QueueManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by yilunq on 12/08/17.
+ *
+ * This is the Main function to execute
+ */
+public class Main {
+    private static Timer timer;
+    private static TimerTask task;
+
+    public static void main(String[] args) {
+
+        // ---------------------------------------------------
+        // NEEDS TO BE MODIFY TO BE "read from File"
+        // OR GET queueID AUTOMATICALLY
+        String queueID = "0090FAE68C221ED78F84917FE2FBA0CB";
+        //----------------------------------------------------
+
+        start(queueID);
+    }
+
+    public static void start(String queueID) {
+        timer = new Timer();
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                QueueManager qm = new QueueManager(queueID);
+                qm.run();
+            }
+        };
+
+        // note, period should be 15 mins
+        timer.schedule(task, 0, 5000);
+    }
+}
---
+++ src/main/java/com/allen/model/IRT.java
@@ -0,0 +1,99 @@
+package com.allen.model;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by yilunq on 12/08/17.
+ *
+ * This class is to parse and analysis IRT
+ */
+
+public class IRT {
+    private String irt;
+    private int year;
+    private int date;
+    private int hour;
+    private int minute;
+
+
+    public IRT(String irt) {
+        this.irt = irt;
+        setVaraibles();
+    }
+
+    private void setVaraibles() {
+        year = Integer.parseInt(irt.substring(0,4));
+        date = Integer.parseInt(irt.substring(4,8));
+        hour = Integer.parseInt(irt.substring(8,10));
+        minute = Integer.parseInt(irt.substring(10,12));
+    }
+
+    public boolean isExpire() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+        String currentDate = dateFormat.format(new Date());
+
+        IRT now = new IRT(currentDate);
+
+        if (now.getYear() > year) {
+            return false;
+        }
+        if (now.getYear() == year) {
+            if (now.getDate() > date) {
+                return false;
+            }
+            if (now.getDate() == date) {
+                // System's 19 hour = 12PST
+                if (now.getHour()+7 > hour) {
+                    return false;
+                }
+                if (now.getHour()+7 == hour) {
+                    if (now.getMinute() > minute) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    // same day but is about expiring
+    // pre-request: minutesLeft between 1 and 59
+    public boolean isLessThan (int minutesLeft) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+        String currentDate = dateFormat.format(new Date());
+
+        IRT now = new IRT(currentDate);
+
+        if (now.getYear() == year && now.getDate() == date
+                && now.getHour() + 7 == hour) { // System's 19 hour = 12PST
+            if (minute - now.getMinute() <= minutesLeft) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    @Override
+    public String toString() {
+        return "[Class IRT = "+"Year = "+year+", Date = "+date+", Hour = "+hour+", Minute = "+minute+"]";
+    }
+}
